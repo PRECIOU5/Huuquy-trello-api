93f1ad40aa2225cd72bd5cec04d56cd50671242d
PRECIOU5 <91598515+PRECIOU5@users.noreply.github.com>
2022-11-17 07:57:31 +0700

    final

--- src/server.js
+++ src/server.js
@@ -1,29 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import { connectDB } from './config/mongodb'
-import { env } from '*/config/environment'
-import { apiV1} from '*/routes/v1'
-import {corsOptions} from '*/config/cors'
-
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/mongodb";
+import { env } from "*/config/environment";
+import { apiV1 } from "*/routes/v1";
+import { corsOptions } from "*/config/cors";
 
 connectDB()
-  .then(() => console.log('đã kết nối thành công'))
+  .then(() => console.log("đã kết nối thành công"))
   .then(() => bootServer())
-  .catch(error => {
-    console.error(error)
-    process.exit(1)
-  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
 const bootServer = () => {
-  const app = express()
+  const app = express();
 
-  app.use(cors(corsOptions))
+  app.use(cors(corsOptions));
 
   //enable req.body data
-  app.use(express.json())
+  app.use(express.json());
   //Use API
-  app.use('/v1', apiV1)
+  app.use("/v1", apiV1);
   app.listen(env.APP_PORT, env.APP_HOST_NAME, () => {
-    console.log(`hello Quy,I am running at ${env.APP_HOSTNAME}:${env.APP_PORT}/`)
-  })
-}
+    console.log(
+      `hello Quy,I am running at ${env.APP_HOSTNAME}:${env.APP_PORT}/`
+    );
+  });
+};
